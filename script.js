4c6bc5b6741123eb4509934034b9e71843ed324c
Leenuri <leenl1016@naver.com>
2014-06-24 00:38:02 +0900

    added

--- script.js
+++ script.js
@@ -2,29 +2,108 @@ $(document).ready(function(){
  
 	show("main");
 	
-	$("#main .menu li").click(function(){
-		show("category")
+
+	//main
+	$("#main .menu li:nth-child(1)").click(function(){
+		show("category-a")
 	});
-	
-	$("#category .top").click(function(){
+	$("#category-a .top").click(function(){
+		show("main");
+	});
+
+	$("#main .menu li:nth-child(2)").click(function(){
+		show("category-d")
+	});	
+	$("#category-d .top").click(function(){
+		show("main");
+	});
+
+	$("#main .menu li:nth-child(3)").click(function(){
+		show("category-g")
+	});	
+	$("#category-g .top").click(function(){
+		show("main");
+	});
+
+	$("#main .menu li:nth-child(4)").click(function(){
+		show("category-j")
+	});	
+	$("#category-j .top").click(function(){
+		show("main");
+	});
+
+	$("#main .menu li:nth-child(5)").click(function(){
+		show("category-m")
+	});	
+	$("#category-m .top").click(function(){
+		show("main");
+	});
+
+	$("#main .menu li:nth-child(6)").click(function(){
+		show("category-p")
+	});	
+	$("#category-p .top").click(function(){
 		show("main");
 	});
 	
-	$("#category .category-menu li").click(function(){
-		show("post")
+	//category
+	$("#category-a .category-menu-a li").click(function(){
+		show("post-a")
+	})
+	$("#category-d .category-menu-d li").click(function(){
+		show("post-d")
+	})
+	$("#category-g .category-menu-g li").click(function(){
+		show("post-g")
+	})
+	$("#category-j .category-menu-j li").click(function(){
+		show("post-j")
+	})
+	$("#category-m .category-menu-m li").click(function(){
+		show("post-m")
+	})
+	$("#category-p .category-menu-p li").click(function(){
+		show("post-p")
 	})
 	
-	$("#post .top").click(function(){
+	//post
+	$("#post-a .top, #post-d .top, #post-g .top, #post-j .top, #post-m .top, #post-p .top,").click(function(){
 		show("category");
 	});
 	
-	$("#post #toggle").click(function(){
+	$("#toggle").click(function(){
 		$("#detail").toggle();
 	})
 
-	$("#user-bar").click(function(){
-		$("#user-more").toggle();
+	//audio-play
+	$("#play-a").click(function(){
+		$("#a")[0].load();
+		$("#a")[0].play();
 	})
+
+	$("#play-d").click(function(){
+		$("#d")[0].load();
+		$("#d")[0].play();
+	})
+	
+	$("#play-g").click(function(){
+		$("#g")[0].load();
+		$("#g")[0].play();
+	})
+	$("#play-j").click(function(){
+		$("#j")[0].load();
+		$("#j")[0].play();
+	})
+	$("#play-m").click(function(){
+		$("#m")[0].load();
+		$("#m")[0].play();
+	})
+	$("#play-p").click(function(){
+		$("#p")[0].load();
+		$("#p")[0].play();
+	})
+
+
 });
  
  
