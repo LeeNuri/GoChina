5373c0928c77415b088ea2484971ed62593428d5
Leenuri <leenl1016@naver.com>
2014-06-23 15:32:11 +0900

    plus

--- script.js
+++ script.js
@@ -25,36 +25,3 @@ var show = function(viewId) {
 	$(".view").fadeOut();
 	$("#"+viewId).fadeIn();
 }
-
-
-
-
-
-/*
-$(document).ready(function(){
-	$("#main .menu li").click(function(){
-		show("category")
-	});
-	$("#category").hide();
-
-	$("#category .top").click(function(){
-		show("main");
-	});
-
-	$("#category .category-menu li").click(function(){
-	show("#post")
-	})
-	$("#post").hide();
-
-	$("#post .top").click(function(){
-		show("main");
-	});
-
-});
-
-var show = function(viewId) {
-	$(".view").fadeOut();
-	$("#"+viewId).fadeIn();
-}
-
-*/
